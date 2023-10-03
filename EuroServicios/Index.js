18f3a2b6fdc51c162a596f30eae0dfc1b91ecee5
Sebastian <sag802204@gmail.com>
2023-10-03 17:29:07 -0500

    Diseño e imagenes

--- EuroServicios/Index.js
+++ EuroServicios/Index.js
@@ -1,37 +1,72 @@
+var app = angular.module('App', [])
+app.controller('InitEncabezado', function($scope){
 
-const inputs = document.querySelectorAll(".inputs");
-
-inputs.forEach(function(input) {
-    input.addEventListener('change', function() {
-        almacenarLocalStorage();
-    });
-});
-
-function almacenarLocalStorage(){
-
-    const PrecioCompraDolar = parseFloat(document.getElementById("PrecioCompraDolar").value).toLocaleString();
-    const PrecioVentaDolar = parseFloat(document.getElementById("PrecioVentaDolar").value).toLocaleString();
-    const PrecioCompraEuro = parseFloat(document.getElementById("PrecioCompraEuro").value).toLocaleString();
-    const PrecioVentaEuro = parseFloat(document.getElementById("PrecioVentaEuro").value).toLocaleString();
-    const PrecioCompraReinoUnido = parseFloat(document.getElementById("PrecioCompraReinoUnido").value).toLocaleString();
-    const PrecioVentaReinoUnido = parseFloat(document.getElementById("PrecioVentaReinoUnido").value).toLocaleString();
-    const PrecioCompraDolarCanadiense = parseFloat(document.getElementById("PrecioCompraDolarCanadiense").value).toLocaleString();
-    const PrecioVentaDolarCanadiense = parseFloat(document.getElementById("PrecioVentaDolarCanadiense").value).toLocaleString();
-
-    const array = 
-    {
-        PrecioCompraDolar,
-        PrecioVentaDolar,
-        PrecioCompraEuro,
-        PrecioVentaEuro,
-        PrecioCompraReinoUnido,
-        PrecioVentaReinoUnido,
-        PrecioCompraDolarCanadiense,
-        PrecioVentaDolarCanadiense
+    $scope.data = {
+        PrecioCompraDolar: '',
+        PrecioVentaDolar: '',
+        PrecioCompraEuro: '',
+        PrecioVentaEuro: '',
+        PrecioCompraReinoUnido: '',
+        PrecioVentaReinoUnido: '',
+        PrecioCompraDolarCanadiense: '',
+        PrecioVentaDolarCanadiense: '',
     }
-       
-    localStorage.setItem('EuroServicios', JSON.stringify(array))
-}
+
+
+
+    $scope.AlmacenarLocalStorage = function(){
+
+        $scope.info = [
+            $scope.data.PrecioCompraDolar = $scope.data.PrecioCompraDolar == "" ? "" : parseFloat($scope.data.PrecioCompraDolar.replaceAll('.','')).toLocaleString("de-DE"),
+            $scope.data.PrecioVentaDolar = $scope.data.PrecioVentaDolar == "" ? "" : parseFloat($scope.data.PrecioVentaDolar.replaceAll('.','')).toLocaleString("de-DE"),
+            $scope.data.PrecioCompraEuro = $scope.data.PrecioCompraEuro == "" ? "" : parseFloat($scope.data.PrecioCompraEuro.replaceAll('.','')).toLocaleString("de-DE"),
+            $scope.data.PrecioVentaEuro = $scope.data.PrecioVentaEuro == "" ? "" : parseFloat($scope.data.PrecioVentaEuro.replaceAll('.','')).toLocaleString("de-DE"),
+            $scope.data.PrecioCompraReinoUnido = $scope.data.PrecioCompraReinoUnido == "" ? "" : parseFloat($scope.data.PrecioCompraReinoUnido.replaceAll('.','')).toLocaleString("de-DE"),
+            $scope.data.PrecioVentaReinoUnido = $scope.data.PrecioVentaReinoUnido == "" ? "" : parseFloat($scope.data.PrecioVentaReinoUnido.replaceAll('.','')).toLocaleString("de-DE"),
+            $scope.data.PrecioCompraDolarCanadiense = $scope.data.PrecioCompraDolarCanadiense == "" ? "" : parseFloat($scope.data.PrecioCompraDolarCanadiense.replaceAll('.','')).toLocaleString("de-DE"),
+            $scope.data.PrecioVentaDolarCanadiense = $scope.data.PrecioVentaDolarCanadiense == "" ? "" : parseFloat($scope.data.PrecioVentaDolarCanadiense.replaceAll('.','')).toLocaleString("de-DE")
+        ];
+            
+       localStorage.setItem('EuroServicios', JSON.stringify($scope.info));
+    }
+
+    $scope.storedLocalStorage = localStorage.getItem('EuroServicios');
+
+    if ($scope.storedLocalStorage) {
+        
+        $scope.info = JSON.parse($scope.storedLocalStorage);
+
+        $scope.data = {
+        PrecioCompraDolar: $scope.info[0],
+        PrecioVentaDolar: $scope.info[1],
+        PrecioCompraEuro: $scope.info[2],
+        PrecioVentaEuro: $scope.info[3],
+        PrecioCompraReinoUnido: $scope.info[4],
+        PrecioVentaReinoUnido: $scope.info[5],
+        PrecioCompraDolarCanadiense: $scope.info[6],
+        PrecioVentaDolarCanadiense: $scope.info[7],
+        }
+    }
+    else{
+        $scope.data;
+    }
+    
+
+    setInterval(function() {
+        document.querySelector('.contenedor').classList.toggle("esconder");
+
+        document.querySelector('.contenedor-transicion').classList.toggle("esconder");
+
+      }, 5000); 
+      
+
+
+})
+    
+    
+    
+    
+    
 
 
 
