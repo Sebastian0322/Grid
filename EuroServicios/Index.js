74dbabd73c5c9c2bb3cc978bcf43b7bd574a02ee
Sebastian <sag802204@gmail.com>
2023-10-10 17:34:07 -0500

    Se agregan cambios

--- EuroServicios/Index.js
+++ EuroServicios/Index.js
@@ -10,58 +10,103 @@ app.controller('InitEncabezado', function($scope){
         PrecioVentaReinoUnido: '',
         PrecioCompraDolarCanadiense: '',
         PrecioVentaDolarCanadiense: '',
+        PrecioCompraBolivar: '',
+        PrecioVentaBolivar: '',
+        PrecioCompraPesoMexicano: '',
+        PrecioVentaPesoMexicano: '',
+        PrecioCompraRealBrasil: '',
+        PrecioVentaRealBrasil: '',
+        PrecioCompraFrancoSuizo: '',
+        PrecioVentaFrancoSuizo: '',
+        PrecioCompraDolarNeolandez: '',
+        PrecioVentaDolarNeolandez: '',
+        PrecioCompraPesoBoliviano: '',
+        PrecioVentaPesoBoliviano: '',
+        FechaActual: ''
     }
 
 
+    $scope.folder1 = false;
+    $scope.folder3 = false;
+    $scope.icono = "Img/pausa.png"
+
+    $scope.diaAnterior = {};
 
     $scope.AlmacenarLocalStorage = function(){
 
-        $scope.info = [
-            $scope.data.PrecioCompraDolar = $scope.data.PrecioCompraDolar == "" ? "" : parseFloat($scope.data.PrecioCompraDolar.replaceAll('.','')).toLocaleString("de-DE"),
-            $scope.data.PrecioVentaDolar = $scope.data.PrecioVentaDolar == "" ? "" : parseFloat($scope.data.PrecioVentaDolar.replaceAll('.','')).toLocaleString("de-DE"),
-            $scope.data.PrecioCompraEuro = $scope.data.PrecioCompraEuro == "" ? "" : parseFloat($scope.data.PrecioCompraEuro.replaceAll('.','')).toLocaleString("de-DE"),
-            $scope.data.PrecioVentaEuro = $scope.data.PrecioVentaEuro == "" ? "" : parseFloat($scope.data.PrecioVentaEuro.replaceAll('.','')).toLocaleString("de-DE"),
-            $scope.data.PrecioCompraReinoUnido = $scope.data.PrecioCompraReinoUnido == "" ? "" : parseFloat($scope.data.PrecioCompraReinoUnido.replaceAll('.','')).toLocaleString("de-DE"),
-            $scope.data.PrecioVentaReinoUnido = $scope.data.PrecioVentaReinoUnido == "" ? "" : parseFloat($scope.data.PrecioVentaReinoUnido.replaceAll('.','')).toLocaleString("de-DE"),
-            $scope.data.PrecioCompraDolarCanadiense = $scope.data.PrecioCompraDolarCanadiense == "" ? "" : parseFloat($scope.data.PrecioCompraDolarCanadiense.replaceAll('.','')).toLocaleString("de-DE"),
-            $scope.data.PrecioVentaDolarCanadiense = $scope.data.PrecioVentaDolarCanadiense == "" ? "" : parseFloat($scope.data.PrecioVentaDolarCanadiense.replaceAll('.','')).toLocaleString("de-DE")
-        ];
-            
-       localStorage.setItem('EuroServicios', JSON.stringify($scope.info));
+        $scope.data = {
+            PrecioCompraDolar: $scope.data.PrecioCompraDolar == "" ? "" : parseFloat($scope.data.PrecioCompraDolar.replaceAll('.','')).toLocaleString("de-DE"),
+            PrecioVentaDolar: $scope.data.PrecioVentaDolar == "" ? "" : parseFloat($scope.data.PrecioVentaDolar.replaceAll('.','')).toLocaleString("de-DE"),
+            PrecioCompraEuro: $scope.data.PrecioCompraEuro == "" ? "" : parseFloat($scope.data.PrecioCompraEuro.replaceAll('.','')).toLocaleString("de-DE"),
+            PrecioVentaEuro: $scope.data.PrecioVentaEuro == "" ? "" : parseFloat($scope.data.PrecioVentaEuro.replaceAll('.','')).toLocaleString("de-DE"),
+            PrecioCompraReinoUnido: $scope.data.PrecioCompraReinoUnido == "" ? "" : parseFloat($scope.data.PrecioCompraReinoUnido.replaceAll('.','')).toLocaleString("de-DE"),
+            PrecioVentaReinoUnido: $scope.data.PrecioVentaReinoUnido == "" ? "" : parseFloat($scope.data.PrecioVentaReinoUnido.replaceAll('.','')).toLocaleString("de-DE"),
+            PrecioCompraDolarCanadiense: $scope.data.PrecioCompraDolarCanadiense == "" ? "" : parseFloat($scope.data.PrecioCompraDolarCanadiense.replaceAll('.','')).toLocaleString("de-DE"),
+            PrecioVentaDolarCanadiense: $scope.data.PrecioVentaDolarCanadiense == "" ? "" : parseFloat($scope.data.PrecioVentaDolarCanadiense.replaceAll('.','')).toLocaleString("de-DE"),
+            PrecioCompraBolivar: $scope.data.PrecioCompraBolivar == "" ? "" : parseFloat($scope.data.PrecioCompraBolivar.replaceAll('.','')).toLocaleString("de-DE"),
+            PrecioVentaBolivar: $scope.data.PrecioVentaBolivar == "" ? "" : parseFloat($scope.data.PrecioVentaBolivar.replaceAll('.','')).toLocaleString("de-DE"),
+            PrecioCompraPesoMexicano: $scope.data.PrecioCompraPesoMexicano == "" ? "" : parseFloat($scope.data.PrecioCompraPesoMexicano.replaceAll('.','')).toLocaleString("de-DE"),
+            PrecioVentaPesoMexicano: $scope.data.PrecioVentaPesoMexicano == "" ? "" : parseFloat($scope.data.PrecioVentaPesoMexicano.replaceAll('.','')).toLocaleString("de-DE"),
+            PrecioCompraRealBrasil: $scope.data.PrecioCompraRealBrasil == "" ? "" : parseFloat($scope.data.PrecioCompraRealBrasil.replaceAll('.','')).toLocaleString("de-DE"),
+            PrecioVentaRealBrasil: $scope.data.PrecioVentaRealBrasil == "" ? "" : parseFloat($scope.data.PrecioVentaRealBrasil.replaceAll('.','')).toLocaleString("de-DE"),
+            PrecioCompraFrancoSuizo: $scope.data.PrecioCompraFrancoSuizo == "" ? "" : parseFloat($scope.data.PrecioCompraFrancoSuizo.replaceAll('.','')).toLocaleString("de-DE"),
+            PrecioVentaFrancoSuizo: $scope.data.PrecioVentaFrancoSuizo == "" ? "" : parseFloat($scope.data.PrecioVentaFrancoSuizo.replaceAll('.','')).toLocaleString("de-DE"),
+            PrecioCompraDolarNeolandez: $scope.data.PrecioCompraDolarNeolandez == "" ? "" : parseFloat($scope.data.PrecioCompraDolarNeolandez.replaceAll('.','')).toLocaleString("de-DE"),
+            PrecioVentaDolarNeolandez: $scope.data.PrecioVentaDolarNeolandez == "" ? "" : parseFloat($scope.data.PrecioVentaDolarNeolandez.replaceAll('.','')).toLocaleString("de-DE"),
+            PrecioCompraPesoBoliviano: $scope.data.PrecioCompraPesoBoliviano == "" ? "" : parseFloat($scope.data.PrecioCompraPesoBoliviano.replaceAll('.','')).toLocaleString("de-DE"),
+            PrecioVentaPesoBoliviano: $scope.data.PrecioVentaPesoBoliviano == "" ? "" : parseFloat($scope.data.PrecioVentaPesoBoliviano.replaceAll('.','')).toLocaleString("de-DE"),
+            FechaActual: new Date()
+        }
+ 
+        localStorage.setItem('EuroServicios', JSON.stringify($scope.data));
     }
 
-    $scope.storedLocalStorage = localStorage.getItem('EuroServicios');
+    $scope.storedLocalStorageActual = localStorage.getItem('EuroServicios');
 
-    if ($scope.storedLocalStorage) {
+    
+    if ($scope.storedLocalStorageActual) {
         
-        $scope.info = JSON.parse($scope.storedLocalStorage);
+        $scope.data = JSON.parse($scope.storedLocalStorageActual);
 
-        $scope.data = {
-        PrecioCompraDolar: $scope.info[0],
-        PrecioVentaDolar: $scope.info[1],
-        PrecioCompraEuro: $scope.info[2],
-        PrecioVentaEuro: $scope.info[3],
-        PrecioCompraReinoUnido: $scope.info[4],
-        PrecioVentaReinoUnido: $scope.info[5],
-        PrecioCompraDolarCanadiense: $scope.info[6],
-        PrecioVentaDolarCanadiense: $scope.info[7],
-        }
+        $scope.data.PrecioCompraDolar;
+        $scope.data.PrecioVentaDolar;
+        $scope.data.PrecioCompraEuro;
+        $scope.data.PrecioVentaEuro;
+        $scope.data.PrecioCompraReinoUnido;
+        $scope.data.PrecioVentaReinoUnido;
+        $scope.data.PrecioCompraDolarCanadiense;
+        $scope.data.PrecioVentaDolarCanadiense;
+        $scope.data.PrecioCompraBolivar;
+        $scope.data.PrecioVentaBolivar;
+        $scope.data.PrecioCompraPesoMexicano;
+        $scope.data.PrecioVentaPesoMexicano;
+        $scope.data.PrecioCompraRealBrasil;
+        $scope.data.PrecioVentaRealBrasil;
+        $scope.data.PrecioCompraFrancoSuizo;
+        $scope.data.PrecioVentaFrancoSuizo;
+        $scope.data.PrecioCompraDolarNeolandez;
+        $scope.data.PrecioVentaDolarNeolandez;
+        $scope.data.PrecioCompraPesoBoliviano;
+        $scope.data.PrecioVentaPesoBoliviano;
     }
     else{
         $scope.data;
     }
+
+
+    $scope.Pausar = function(){
+       
+        $scope.folder1 = !$scope.folder1;
+        $scope.folder3 = !$scope.folder3;
+        $scope.icono = $scope.folder1 || $scope.folder3 == true ? "Img/continuar.png" : "Img/pausa.png"; 
+    }
+   
+
     
 
-    // setInterval(function() {
-    //     document.querySelector('.contenedor').classList.toggle("esconder");
-
-    //     document.querySelector('.contenedor-transicion').classList.toggle("esconder");
-
-    //   }, 5000); 
-      
-
-
 })
+
+
     
     
     
